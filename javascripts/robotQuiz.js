3f55bd9441372628fd6e1d11a92d414b4e51680b
Tommy Waters <Thomas.Waters05@gmail.com>
2016-10-22 10:32:43 -0500

    added html dividers, added beginning of main function- mainly testing gitignore file to ensure accurate

--- javascripts/robotQuiz.js
+++ javascripts/robotQuiz.js
@@ -1,2 +1,17 @@
 "use strict";
 
+var RobotGame = (function(baseRoboFunction){
+
+  baseRoboFunction.Robotz = {};
+
+  //Create a base function for robot/player
+
+   baseRoboFunction.Robotz.Player = function(name) {
+    this.class = null;
+    this.weapon = null;
+
+    this.PlayerName = name || "Robo Dude";
+    this.health = Math.floor(Math.random() * 30 + 100);
+    this.strength = 75;
+   };
+});
